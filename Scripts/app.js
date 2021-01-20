ca3cec50f06db4ebd74e7c14a07e5819bb0e5c66
Brett Tindall <sub4subm8.exe@gmail.com>
2021-01-20 18:00:53 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+/* Custom Javascript goes here */
+
+//IIFE - Immediately Invoked Function
+
+(function()
+{   
+    function start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load",start);
+})();
